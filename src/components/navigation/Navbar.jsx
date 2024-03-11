611b421e235fe9fd72a2adadc013d39dbab5630f
Patrick Dolan <dolanp1992@gmail.com>
2024-03-11 13:41:28 -0700

    update styes, mobile handlers, and logo link

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -7,23 +7,29 @@ import CloseIcon from "../../assets/shared/mobile/CloseIcon"
 function Navbar() {
   const [mobileNavOpen, setMobileNavOpen] = useState(false)
 
-  const toggleMobileNav = () => {
-    setMobileNavOpen(prev => !prev);
+  const openMobileNav = () => {
+    setMobileNavOpen(true);
+  }
+
+  const closeMobileNav = () => {
+    setMobileNavOpen(false);
   }
 
   return (
     <>
       <nav className="fixed w-full bg-white z-30">
         <div className="flex justify-between items-center px-6 min-h-24">
-          <img src="/designo-website/shared/desktop/logo-dark.png" alt="" className="w-nav-logo" />
-          <button onClick={toggleMobileNav}>
+          <Link to="/designo-website/" onClick={closeMobileNav}>
+            <img src="/designo-website/shared/desktop/logo-dark.png" alt="Designo logo." className="w-nav-logo" />
+          </Link>
+          <button onClick={mobileNavOpen ? closeMobileNav : openMobileNav}>
             {mobileNavOpen ? <CloseIcon /> : <HamburgerIcon />}
           </button>
         </div>
-        <ul className={`uppercase px-6 py-12 text-2xl leading-normal space-y-7 bg-black text-white ${mobileNavOpen ? "" : "hidden"}`}>
-          <li><Link to="/designo-website/about" onClick={toggleMobileNav}>Our company</Link></li>
-          <li><Link to="/designo-website/locations" onClick={toggleMobileNav}>Locations</Link></li>
-          <li><Link to="/designo-website/contact" onClick={toggleMobileNav}>Contact</Link></li>
+        <ul className={`uppercase px-6 py-12 text-2xl space-y-8 leading-6 tracking-widest bg-black text-white ${mobileNavOpen ? "" : "hidden"}`}>
+          <li><Link to="/designo-website/about" onClick={closeMobileNav}>Our company</Link></li>
+          <li><Link to="/designo-website/locations" onClick={closeMobileNav}>Locations</Link></li>
+          <li><Link to="/designo-website/contact" onClick={closeMobileNav}>Contact</Link></li>
         </ul>
       </nav>
       <div className={`fixed inset-0 bg-black opacity-50 z-20 ${mobileNavOpen ? "" : "hidden"}`}></div>
