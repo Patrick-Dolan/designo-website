ec559cf574e52ecabcf3ad30e3e0de2b446e742d
Patrick Dolan <dolanp1992@gmail.com>
2024-03-11 16:48:08 -0700

    add footer to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,24 @@ import HomePage from "./pages/HomePage"
 import AboutPage from "./pages/AboutPage"
 import LocationsPage from "./pages/LocationsPage"
 import ContactPage from "./pages/ContactPage"
+import Footer from "./shared/Footer"
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <main className="px-6 ">
-        <Routes>
-          <Route path="/designo-website/" element={<HomePage />} />
-          <Route path="/designo-website/about" element={<AboutPage />} />
-          <Route path="/designo-website/locations" element={<LocationsPage />} />
-          <Route path="/designo-website/contact" element={<ContactPage />} />
-          <Route path="*" element={<div>404</div>} />
-        </Routes>
-      </main>
+    <div className="flex flex-col justify-between h-screen">
+      <div>
+        <Navbar />
+        <main className="px-6">
+          <Routes>
+            <Route path="/designo-website/" element={<HomePage />} />
+            <Route path="/designo-website/about" element={<AboutPage />} />
+            <Route path="/designo-website/locations" element={<LocationsPage />} />
+            <Route path="/designo-website/contact" element={<ContactPage />} />
+            <Route path="*" element={<div>404</div>} />
+          </Routes>
+        </main>
+      </div>
+      <Footer />
     </div>
   )
 }
