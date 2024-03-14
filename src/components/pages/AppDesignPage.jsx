cef65774f3d7f7e4b162aa584e4090f8e1d0f0ec
Patrick Dolan <dolanp1992@gmail.com>
2024-03-14 11:36:49 -0700

    add projects to AppDesignPage

--- src/components/pages/AppDesignPage.jsx
+++ src/components/pages/AppDesignPage.jsx
@@ -1,14 +1,46 @@
 import LinkCards from "../navigation/LinkCards"
 import Container from "../shared/Container"
 import DesignPageHero from "../shared/DesignPageHero"
+import ProjectList from "../shared/ProjectList"
 
 function AppDesignPage() {
+  const projects =[
+    {
+      title: "Airfilter",
+      description: "Solving the problem of poor indoor air quality by filtering the air",
+      image: "/designo-website/assets/app-design/desktop/image-airfilter.jpg"
+    },
+    {
+      title: "Eyecam",
+      description: "Product that lets you edit your favorite photos and videos at any time",
+      image: "/designo-website/assets/app-design/desktop/image-eyecam.jpg"
+    },
+    {
+      title: "Faceit",
+      description: "Get to meet your favorite internet superstar with the faceit app",
+      image: "/designo-website/assets/app-design/desktop/image-faceit.jpg"
+    },
+    {
+      title: "Todo",
+      description: "A todo app that features cloud sync with light and dark mode",
+      image: "/designo-website/assets/app-design/desktop/image-todo.jpg"
+    },
+    {
+      title: "Loopstudios",
+      description: "A VR experience app made for Loopstudios",
+      image: "/designo-website/assets/app-design/desktop/image-loopstudios.jpg"
+    }
+  ];
+
   return (
     <>
       <div className="mb-24">
         <DesignPageHero />
       </div>
       <Container>
+        <div className="mb-24">
+          <ProjectList projects={projects} />
+        </div>
         <LinkCards />
       </Container>
     </>
