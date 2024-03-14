b6e10b7b650764328bdf4c7f080c26925d327736
Patrick Dolan <dolanp1992@gmail.com>
2024-03-14 11:32:56 -0700

    add projects to GraphicDesignPage

--- src/components/pages/GraphicDesignPage.jsx
+++ src/components/pages/GraphicDesignPage.jsx
@@ -1,14 +1,36 @@
 import LinkCards from "../navigation/LinkCards"
 import Container from "../shared/Container"
 import DesignPageHero from "../shared/DesignPageHero"
+import ProjectList from "../shared/ProjectList"
 
 function GraphicDesignPage() {
+  const projects =[
+    {
+      title: "Tim Brown",
+      description: "A book cover designed for Tim Brown's new release, 'Change'",
+      image: "/designo-website/assets/graphic-design/desktop/image-change.jpg"
+    },
+    {
+      title: "Boxed Water",
+      description: "A simple packaging concept made for Boxed Water",
+      image: "/designo-website/assets/graphic-design/desktop/image-boxed-water.jpg"
+    },
+    {
+      title: "Science!",
+      description: "A poster made in collaboration with the Federal Art Project",
+      image: "/designo-website/assets/graphic-design/desktop/image-science.jpg"
+    },
+  ];
+
   return (
     <>
       <div className="mb-24">
         <DesignPageHero />
       </div>
       <Container>
+      <div className="mb-24">
+          <ProjectList projects={projects} />
+        </div>
         <LinkCards />
       </Container>
     </>
