86af507b813c49fda8973a05f7fc14101f25ccf3
Patrick Dolan <dolanp1992@gmail.com>
2024-03-14 11:20:06 -0700

    add ProjectList and state to component

--- src/components/pages/WebDesignPage.jsx
+++ src/components/pages/WebDesignPage.jsx
@@ -1,14 +1,51 @@
 import LinkCards from "../navigation/LinkCards"
 import Container from "../shared/Container"
 import DesignPageHero from "../shared/DesignPageHero"
+import ProjectList from "../shared/ProjectList"
 
 function WebDesignPage() {
+  const projects =[
+    {
+      title: "Express",
+      description: "A multi-carrier shipping website for ecommerce businesses",
+      image: "/designo-website/assets/web-design/desktop/image-express.jpg"
+    },
+    {
+      title: "Transfer",
+      description: "Site for low-cost money transfers and sending money within seconds",
+      image: "/designo-website/assets/web-design/desktop/image-transfer.jpg"
+    },
+    {
+      title: "Photon",
+      description: "A state-of-the-art music player with high-resolution audio and DSP effects",
+      image: "/designo-website/assets/web-design/desktop/image-photon.jpg"
+    },
+    {
+      title: "Builder",
+      description: "Connects users with local contractors based on their location",
+      image: "/designo-website/assets/web-design/desktop/image-builder.jpg"
+    },
+    {
+      title: "Blogr",
+      description: "Blogr is a platform for creating an online blog or publication",
+      image: "/designo-website/assets/web-design/desktop/image-blogr.jpg"
+    },
+    {
+      title: "Camp",
+      description: "Get expert training in coding, data, design, and digital marketing",
+      image: "/designo-website/assets/web-design/desktop/image-camp.jpg"
+    }
+  ];
+
   return (
     <>
       <div className="mb-24">
         <DesignPageHero />
       </div>
       <Container>
+        <div className="mb-24">
+          <ProjectList projects={projects} />
+        </div>
         <LinkCards />
       </Container>
     </>
