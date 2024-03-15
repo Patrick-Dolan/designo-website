0f376ce6816a9ef136abc02556d1dc56f67dbbb7
Patrick Dolan <dolanp1992@gmail.com>
2024-03-15 10:45:49 -0700

    update background position name

--- src/components/shared/FooterContactCard.jsx
+++ src/components/shared/FooterContactCard.jsx
@@ -14,7 +14,7 @@ function FooterContactCard() {
     <div className="relative mt-30">
       <div className="bg-black h-[53%] absolute w-full -bottom-[1px]"></div>
       <Container>
-        <CardTemplate className="relative mb-2 bg-contact-card-pattern bg-contact-card bg-no-repeat py-16">
+        <CardTemplate className="relative mb-2 bg-contact-card-pattern bg-footer-contact-card-pos bg-no-repeat py-16">
           <div className="text-white flex flex-col gap-8">
             <h2 className="text-center font-medium">Let&apos;s talk about your project.</h2>
             <p className="text-center text-sm">Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
