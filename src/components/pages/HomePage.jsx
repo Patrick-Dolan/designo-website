3a19393e0997a2cf4158f8498b697ac8a892b6ce
Patrick Dolan <dolanp1992@gmail.com>
2024-03-19 08:27:32 -0700

    set up tablet homepage padding

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -7,11 +7,11 @@ function HomePage() {
   return (
     <>
       <div className="mb-30">
-        <Container className="px-0 md:px-6">
+        <Container className="px-0 md:px-10">
           <HomeHeroSection />
         </Container>
       </div>
-      <Container>
+      <Container className="px-6 md:px-10">
         <div className="mb-30">
           <LinkCards />
         </div>
