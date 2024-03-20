5380e75ebaa6efb06e6e9df2c4092e0962fd52e3
Patrick Dolan <dolanp1992@gmail.com>
2024-03-20 08:31:51 -0700

    add tablet responsiveness and styles

--- src/components/about/AboutHeroSection.jsx
+++ src/components/about/AboutHeroSection.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
 import Container from "../shared/Container"
 
 function AboutHeroSection() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const getImageSrc = () => {
+    if (windowWidth >= 1280) {
+      return "/designo-website/assets/about/desktop/image-about-hero.jpg";
+    } else if (windowWidth >= 768) {
+      return "/designo-website/assets/about/tablet/image-about-hero.jpg";
+    } else {
+      return "/designo-website/assets/about/mobile/image-about-hero.jpg";
+    }
+  };
+
   return (
-    <div className="bg-primary bg-sm-about-hero-pattern bg-sm-about-hero-pos bg-no-repeat rounded-none text-white text-center overflow-hidden md:rounded-2xl">
-      <img src="/designo-website/assets/about/mobile/image-about-hero.jpg" className="mx-auto" alt="" />
-      <div className="py-20">
+    <div className="bg-primary bg-sm-about-hero-pattern bg-sm-about-hero-pos bg-no-repeat rounded-none text-white text-center overflow-hidden md:rounded-2xl md:bg-md-about-hero-pattern md:bg-md-about-hero-pos">
+      <img src={getImageSrc()} className="mx-auto w-full object-cover h-80" alt="" />
+      <div className="py-20 md:py-16">
         <Container>
-          <h1 className="font-medium mb-6">About Us</h1>
-          <p className="text-15px">Founded in 2010, we are a creative agency that produces lasting results for our clients. We’ve partnered with many startups, corporations, and nonprofits alike to craft designs that make real impact. We’re always looking forward to creating brands, products, and digital experiences that connect with our clients’ audiences.</p>
+          <h1 className="font-medium mb-6 md:text-5xl md:mb-8">About Us</h1>
+          <p className="text-15px md:text-base md:max-w-143 md:mx-auto">Founded in 2010, we are a creative agency that produces lasting results for our clients. We&apos;ve partnered with many startups, corporations, and nonprofits alike to craft designs that make real impact. We&apos;re always looking forward to creating brands, products, and digital experiences that connect with our clients&apos; audiences.</p>
         </Container>
       </div>
     </div>
--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -1,18 +1,49 @@
+import { useState, useEffect } from "react"
 import AboutHeroSection from "../about/AboutHeroSection"
 import Container from "../shared/Container"
 import LocationLinks from "../shared/LocationLinks"
 
 function AboutPage() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  let talentImageSrc = "/designo-website/assets/about/mobile/image-world-class-talent.jpg";
+  let realDealImageSrc = "/designo-website/assets/about/mobile/image-real-deal.jpg";
+
+  if (windowWidth >= 1280) {
+    talentImageSrc = "/designo-website/assets/about/desktop/image-world-class-talent.jpg";
+    realDealImageSrc = "/designo-website/assets/about/desktop/image-real-deal.jpg";
+  } else if (windowWidth >= 768) {
+    talentImageSrc = "/designo-website/assets/about/tablet/image-world-class-talent.jpg";
+    realDealImageSrc = "/designo-website/assets/about/tablet/image-real-deal.jpg";
+  } else {
+    talentImageSrc = "/designo-website/assets/about/mobile/image-world-class-talent.jpg";
+    realDealImageSrc = "/designo-website/assets/about/mobile/image-real-deal.jpg";
+  }
+
   return (
-    <div>
-      <AboutHeroSection />
+    <div className="md:mx-10">
+      <div className="mb-30">
+        <AboutHeroSection />
+      </div>
       <div className="bg-light-gray rounded-none text-center overflow-hidden md:rounded-2xl">
-        <img src="/designo-website/assets/about/mobile/image-world-class-talent.jpg" alt="" />
+        <img src={talentImageSrc} className="mx-auto w-full object-cover h-80" alt="" />
         <div className="py-20 bg-three-circles bg-no-repeat">
           <Container>
-            <h2 className="font-medium mb-6 text-primary">World-class talent</h2>
-            <p className="text-15px mb-6">We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.</p>
-            <p className="text-15px">Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission.</p>
+            <h2 className="font-medium mb-6 text-primary md:text-10 md:leading-10">World-class talent</h2>
+            <p className="text-15px mb-10 md:text-base md:max-w-143 md:mx-auto">We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.</p>
+            <p className="text-15px md:text-base md:max-w-143 md:mx-auto">Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission.</p>
           </Container>
         </div>
       </div>
@@ -20,12 +51,12 @@ function AboutPage() {
         <LocationLinks />
       </div>
       <div className="bg-light-gray rounded-none text-center overflow-hidden md:rounded-2xl">
-        <img src="/designo-website/assets/about/mobile/image-real-deal.jpg" alt="" />
+        <img src={realDealImageSrc} className="mx-auto w-full object-cover h-80" alt="" />
         <div className="py-20 bg-three-circles bg-no-repeat">
           <Container>
-            <h2 className="font-medium mb-6 text-primary">The real deal</h2>
-            <p className="text-15px mb-6">As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.</p>
-            <p className="text-15px">We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.</p>
+            <h2 className="font-medium mb-6 text-primary md:text-10 md:leading-10">The real deal</h2>
+            <p className="text-15px mb-6 md:text-base md:max-w-143 md:mx-auto">As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.</p>
+            <p className="text-15px md:text-base md:max-w-143 md:mx-auto">We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.</p>
           </Container>
         </div>
       </div>
