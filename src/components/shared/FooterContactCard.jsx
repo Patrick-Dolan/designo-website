62bf2bb459ede0531314b409a4bb35e6f9644727
Patrick Dolan <dolanp1992@gmail.com>
2024-04-17 11:49:28 -0700

    set footer container large spacing

--- src/components/shared/FooterContactCard.jsx
+++ src/components/shared/FooterContactCard.jsx
@@ -13,7 +13,7 @@ function FooterContactCard() {
   return (
     <div className="relative mt-30">
       <div className="bg-black h-[53%] absolute w-full -bottom-[1px] md:-bottom-[2px] md:h-[30%]"></div>
-      <Container className="px-6 md:px-10">
+      <Container className="mx-6 md:mx-10 lg:mx-41">
         <CardTemplate className="relative mb-2 bg-contact-card-pattern bg-footer-contact-card-pos bg-no-repeat py-16 md:py-15 md:bg-md-footer-contact-card-pos">
           <div className="text-white flex flex-col gap-8 md:px-15">
             <h2 className="text-center font-medium md:text-10 md:max-w-84 md:mx-auto">Let&apos;s talk about your project.</h2>
