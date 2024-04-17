e42e1bf9bb80f2c417bf0b2cac1bd92cfcda777b
Patrick Dolan <dolanp1992@gmail.com>
2024-04-17 15:39:22 -0700

    update footer for large breakpoint

--- src/components/shared/FooterContactCard.jsx
+++ src/components/shared/FooterContactCard.jsx
@@ -11,14 +11,16 @@ function FooterContactCard() {
   }
 
   return (
-    <div className="relative mt-30">
+    <div className="relative mt-30 lg:mt-40">
       <div className="bg-black h-[53%] absolute w-full -bottom-[1px] md:-bottom-[2px] md:h-[30%]"></div>
       <Container className="mx-6 md:mx-10 lg:mx-41">
-        <CardTemplate className="relative mb-2 bg-contact-card-pattern bg-footer-contact-card-pos bg-no-repeat py-16 md:py-15 md:bg-md-footer-contact-card-pos">
-          <div className="text-white flex flex-col gap-8 md:px-15">
-            <h2 className="text-center font-medium md:text-10 md:max-w-84 md:mx-auto">Let&apos;s talk about your project.</h2>
-            <p className="text-center text-sm md:text-base">Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
-            <div className="flex justify-center">
+        <CardTemplate className="relative mb-2 bg-contact-card-pattern bg-footer-contact-card-pos bg-no-repeat py-16 md:py-15 md:bg-md-footer-contact-card-pos lg:py-18 lg:bg-right">
+          <div className="text-white flex flex-col gap-8 md:px-15 lg:flex-row lg:justify-between">
+            <div className="text-center flex flex-col gap-8 lg:text-left">
+              <h2 className="font-medium md:text-10 md:max-w-84 md:mx-auto lg:mx-0 lx:max-w-64">Let&apos;s talk about your project.</h2>
+              <p className="text-sm md:text-base lg:max-w-108">Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
+            </div>
+            <div className="mx-auto lg:mx-0 lg:my-auto">
               <Button text="Get in touch" type="button" onClick={handleContactClick} variant="secondary" />
             </div>
           </div>
