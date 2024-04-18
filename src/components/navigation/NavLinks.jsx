2c664248601aa166f42fa873c09ff73188c25597
Patrick Dolan <dolanp1992@gmail.com>
2024-04-18 08:17:33 -0700

    fix link hover for small screens

--- src/components/navigation/NavLinks.jsx
+++ src/components/navigation/NavLinks.jsx
@@ -5,9 +5,9 @@ function NavLinks({ mobileNavOpen, closeMobileNav, className}) {
   return (
     <div className={`${className} ${mobileNavOpen ? "" : "hidden"}`}>
       <ul className={`uppercase px-6 py-12 text-2xl space-y-8 leading-6 tracking-widest bg-black text-white md:bg-white md:text-black md:flex md:text-sm md:items-center md:space-y-0 md:gap-10 md:p-0 md:tracking-2px`}>
-        <li className="hover:border-b"><Link to="/designo-website/about" onClick={closeMobileNav}>Our company</Link></li>
-        <li className="hover:border-b"><Link to="/designo-website/locations" onClick={closeMobileNav}>Locations</Link></li>
-        <li className="hover:border-b"><Link to="/designo-website/contact" onClick={closeMobileNav}>Contact</Link></li>
+        <li><Link to="/designo-website/about" className="hover:border-b" onClick={closeMobileNav}>Our company</Link></li>
+        <li><Link to="/designo-website/locations" className="hover:border-b" onClick={closeMobileNav}>Locations</Link></li>
+        <li><Link to="/designo-website/contact" className="hover:border-b" onClick={closeMobileNav}>Contact</Link></li>
       </ul>
     </div>
   )
